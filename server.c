8cdcadb2f9153b46343ac660f96f8f42f910d094
Fernanda Sánchez <mafsr1999@gmail.com>
2021-06-09 17:41:55 -0500

    Load sequences and reference files

--- server.c
+++ server.c
@@ -12,6 +12,20 @@
 
 char cwd[FILENAME_MAX];
 
+int countChar(char* clientMsg){
+  int iCount = 0;
+  char ch;
+  FILE *ptr = fopen(clientMsg, "r");
+  if(ptr == NULL) return -1;
+  while((ch = fgetc(ptr))!=EOF){
+      iCount++;
+  }
+  iCount+=10;
+  fclose(ptr);
+  return iCount;
+}
+
+
 static void daemonize()
 {
   pid_t pid;
@@ -78,6 +92,12 @@ int main()
   struct sockaddr_in server, client;
   char clientMsg[1024] = {0};
   char serverReply[1024] = {0};
+  char *reference;
+  char cOpt; 
+  int referenceSize;
+  char secuencia[100000];
+  int cantSeq = 0;
+  
 
   printf("Starting daemonize\n");
   daemonize();
@@ -116,9 +136,57 @@ int main()
 
       while (recv(new_socket, clientMsg, sizeof(clientMsg), 0) > 0)
       {
+        //char clientMsg[1000] = "test_files/";
+        int iLen = strlen(clientMsg);
+        cOpt = clientMsg[iLen-1];
 
-        snprintf(serverReply, sizeof(serverReply), "Welcome");
-        send(new_socket, serverReply, sizeof(serverReply), 0);
+        switch (cOpt)
+        {
+          case 'R': // LOAD NEW REFERENCE 
+            clientMsg[iLen-1]='\0';
+            //strcat(clientMsg, clientMsg);
+            referenceSize = countChar(clientMsg);
+            reference = malloc(referenceSize*sizeof(char));
+
+            FILE *ptrR = fopen(clientMsg, "r");
+
+            if(ptrR == NULL) syslog(LOG_ERR, "Could no open selected file\n");
+            else{
+              if(fgets(reference, referenceSize, ptrR)==NULL){
+                syslog(LOG_ERR, "Error trying to read from file");
+                snprintf(serverReply, sizeof(serverReply), "Reference could not be loaded");
+              }else{
+                snprintf(serverReply, sizeof(serverReply), "Reference loaded!");
+                send(new_socket, serverReply, sizeof(serverReply), 0);
+              }
+            }
+
+          break;
+
+          case 'S': //LOAD NEW SEQUENCES 
+            cantSeq = 0;
+            clientMsg[iLen-1]='\0';
+            
+            referenceSize = countChar(clientMsg);
+            reference = malloc(referenceSize*sizeof(char));
+
+            FILE *ptrS = fopen(clientMsg, "r");
+
+            if(ptrS == NULL) syslog(LOG_ERR, "Could no open selected file\n");
+            else{
+              while( fgets (secuencia, 1024, ptrS) ){
+                /*
+                  AQUI HAY QUE GUARDAR CADA secuencia EN UN STRUCT
+                */
+                cantSeq++;
+              }
+            }
+              
+              snprintf(serverReply, sizeof(serverReply), "Sequences loaded!");
+              send(new_socket, serverReply, sizeof(serverReply), 0);
+          break;  
+        } 
+        
       }
     }
     if (new_socket < 0)
