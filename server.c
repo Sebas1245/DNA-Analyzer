15fa3deed28a08005f868c77107f137788cc9004
Angel-AG <miniprobecat@hotmail.com>
2021-06-11 20:48:14 -0500

    Fix parallel for and add default case in switch statement

--- server.c
+++ server.c
@@ -1,21 +1,20 @@
-#include <arpa/inet.h> //inet_addr
+#include <arpa/inet.h>  //inet_addr
 #include <limits.h>
+#include <omp.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h> //strlen
+#include <string.h>  //strlen
 #include <sys/socket.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <syslog.h>
-#include <unistd.h> //write
-#include <omp.h>
+#include <unistd.h>  //write
 
 char cwd[FILENAME_MAX];
 
 // structure to store the sequences
-typedef struct
-{
+typedef struct {
   char *sequence;
   char *found_at_reference_address;
   int found_at;
@@ -23,15 +22,12 @@ typedef struct
 } sequence_string;
 
 // contar los
-int countChar(char *clientMsg)
-{
+int countChar(char *clientMsg) {
   int iCount = 0;
   char ch;
   FILE *ptr = fopen(clientMsg, "r");
-  if (ptr == NULL)
-    return -1;
-  while ((ch = fgetc(ptr)) != EOF)
-  {
+  if (ptr == NULL) return -1;
+  while ((ch = fgetc(ptr)) != EOF) {
     iCount++;
   }
   iCount += 10;
@@ -39,23 +35,19 @@ int countChar(char *clientMsg)
   return iCount;
 }
 
-static void daemonize()
-{
+static void daemonize() {
   pid_t pid;
   /* Fork off the parent process */
   pid = fork();
 
   /* An error occurred */
-  if (pid < 0)
-    exit(EXIT_FAILURE);
+  if (pid < 0) exit(EXIT_FAILURE);
 
   /* Success: Let the parent terminate */
-  if (pid > 0)
-    exit(EXIT_SUCCESS);
+  if (pid > 0) exit(EXIT_SUCCESS);
 
   /* On success: The child process becomes session leader */
-  if (setsid() < 0)
-    exit(EXIT_FAILURE);
+  if (setsid() < 0) exit(EXIT_FAILURE);
 
   /* Catch, ignore and handle signals */
   /*TODO: Implement a working signal handler */
@@ -66,25 +58,21 @@ static void daemonize()
   pid = fork();
 
   /* An error occurred */
-  if (pid < 0)
-    exit(EXIT_FAILURE);
+  if (pid < 0) exit(EXIT_FAILURE);
 
   /* Success: Let the parent terminate */
-  if (pid > 0)
-    exit(EXIT_SUCCESS);
+  if (pid > 0) exit(EXIT_SUCCESS);
 
   /* Set new file permissions */
   umask(027);
 
   /* Change the working directory to the root directory */
   /* or another appropriated directory */
-  if (chdir(getcwd(cwd, sizeof(cwd))) != 0)
-    exit(EXIT_FAILURE);
+  if (chdir(getcwd(cwd, sizeof(cwd))) != 0) exit(EXIT_FAILURE);
 
   /* Close all open file descriptors */
   int x;
-  for (x = sysconf(_SC_OPEN_MAX); x >= 0; x--)
-  {
+  for (x = sysconf(_SC_OPEN_MAX); x >= 0; x--) {
     close(x);
   }
 
@@ -92,15 +80,13 @@ static void daemonize()
   openlog("DNAAnalyzer", LOG_PID, LOG_DAEMON);
 }
 
-void kill_handler(int signal)
-{
+void kill_handler(int signal) {
   syslog(LOG_NOTICE, "Server terminated.");
   closelog();
   exit(0);
 }
 
-int main()
-{
+int main() {
   int socket_desc, new_socket, c;
   struct sockaddr_in server, client;
   char clientMsg[1024] = {0};
@@ -119,13 +105,11 @@ int main()
   daemonize();
   syslog(LOG_NOTICE, "DNA Analyzer Server daemon running.");
 
-  while (1)
-  {
+  while (1) {
     // TODO: Insert daemon code here.
     // Create socket
     socket_desc = socket(AF_INET, SOCK_STREAM, 0);
-    if (socket_desc == -1)
-    {
+    if (socket_desc == -1) {
       syslog(LOG_ERR, "Could not create socket");
       return 1;
     }
@@ -135,8 +119,7 @@ int main()
     server.sin_port = htons(8888);
 
     // Bind
-    if (bind(socket_desc, (struct sockaddr *)&server, sizeof(server)) < 0)
-    {
+    if (bind(socket_desc, (struct sockaddr *)&server, sizeof(server)) < 0) {
       syslog(LOG_ERR, "connect error");
       return 1;
     }
@@ -146,133 +129,136 @@ int main()
     c = sizeof(struct sockaddr_in);
 
     while ((new_socket = accept(socket_desc, (struct sockaddr *)&client,
-                                (socklen_t *)&c)))
-    {
+                                (socklen_t *)&c))) {
       syslog(LOG_NOTICE, "Connection accepted");
 
-      while (recv(new_socket, clientMsg, sizeof(clientMsg), 0) > 0)
-      {
-        //char clientMsg[1000] = "test_files/";
+      while (recv(new_socket, clientMsg, sizeof(clientMsg), 0) > 0) {
+        // char clientMsg[1000] = "test_files/";
         int iLen = strlen(clientMsg);
         cOpt = clientMsg[iLen - 1];
 
-        switch (cOpt)
-        {
-        case 'R': // LOAD NEW REFERENCE
-          clientMsg[iLen - 1] = '\0';
-          //strcat(clientMsg, clientMsg);
-          referenceSize = countChar(clientMsg);
-          reference = malloc(referenceSize * sizeof(char));
+        switch (cOpt) {
+          case 'R':  // LOAD NEW REFERENCE
+            clientMsg[iLen - 1] = '\0';
+            // strcat(clientMsg, clientMsg);
+            referenceSize = countChar(clientMsg);
+            reference = malloc(referenceSize * sizeof(char));
 
-          FILE *ptrR = fopen(clientMsg, "r");
+            FILE *ptrR = fopen(clientMsg, "r");
 
-          if (ptrR == NULL)
-            syslog(LOG_ERR, "Could no open selected file\n");
-          else
-          {
-            if (fgets(reference, referenceSize, ptrR) == NULL)
-            {
-              syslog(LOG_ERR, "Error trying to read from file");
-              snprintf(serverReply, sizeof(serverReply), "Reference could not be loaded");
-            }
-            else
-            {
-              snprintf(serverReply, sizeof(serverReply), "Reference loaded!\n");
-              send(new_socket, serverReply, sizeof(serverReply), 0);
-            }
-            fclose(ptrR);
-          }
-
-        break;
-
-        case 'S': //LOAD NEW SEQUENCES
-          cantSeq = 0;
-          clientMsg[iLen - 1] = '\0';
-          
-          sequenceSize = countChar(clientMsg);
-          secuencia = malloc(sequenceSize * sizeof(char));
-
-          FILE *ptrS = fopen(clientMsg, "r");
-
-          if (ptrS == NULL)
-            syslog(LOG_ERR, "Could no open selected file\n");
-          else
-          {
-
-            while (fgets(secuencia, sequenceSize, ptrS))
-            {
-              /*
-                  AQUI HAY GUARDAMOS CADA secuencia EN UN STRUCT
-                */
-              int read_len = strlen(secuencia);
-              if (secuencia[read_len - 1] == '\n')
-              {
-                secuencia[read_len - 1] = 0;
-                secuencia[read_len - 2] = 0;
+            if (ptrR == NULL)
+              syslog(LOG_ERR, "Could no open selected file\n");
+            else {
+              if (fgets(reference, referenceSize, ptrR) == NULL) {
+                syslog(LOG_ERR, "Error trying to read from file");
+                snprintf(serverReply, sizeof(serverReply),
+                         "Reference could not be loaded");
+              } else {
+                snprintf(serverReply, sizeof(serverReply),
+                         "Reference loaded!\n");
+                send(new_socket, serverReply, sizeof(serverReply), 0);
               }
-              int true_len = strlen(secuencia);
-              // syslog(LOG_NOTICE, "strlen secuencia = %d", true_len);
-              // syslog(LOG_NOTICE, "Secuencia[%d] = %s", cantSeq, secuencia);
-              // syslog(LOG_NOTICE, "Secuencia[%d] = %s", cantSeq, secuencia);
-              sequences_to_analyze[cantSeq].sequence = malloc(true_len * sizeof(char));
-              strcpy(sequences_to_analyze[cantSeq].sequence, secuencia);
-              sequences_to_analyze[cantSeq].length = true_len;
-              cantSeq++;
+              fclose(ptrR);
             }
-            fclose(ptrS);
-          }
-          free(secuencia);
-          // Begin multithread with Open MP
-          omp_set_num_threads(cantSeq);
 
-          #pragma omp for
-          for (int i = 0; i < cantSeq; i++)
-          {
-            sequences_to_analyze[i].found_at_reference_address = strstr(reference, sequences_to_analyze[i].sequence);
-            if (sequences_to_analyze[i].found_at_reference_address != NULL)
-              sequences_to_analyze[i].found_at = sequences_to_analyze[i].found_at_reference_address - reference;
-            else
-              sequences_to_analyze[i].found_at = -1; // SEQUENCE NOT FOUND
-          }
-          // Sequential code begins again
-          char *sReport = malloc(10000);
-          memset(sReport, '\0', sizeof(sReport));
-          for (int i = 0; i < cantSeq; i++)
-          {
-            char sReportLine[sequences_to_analyze[i].length + 40]; // Buffer for each line that must be printed in report
-            if (sequences_to_analyze[i].found_at == -1)
-            {
-              syslog(5, "%s no se encontro\n", sequences_to_analyze[i].sequence);
-              snprintf(sReportLine, sizeof(sReportLine), "%s no se encontro\n", sequences_to_analyze[i].sequence);
-            }
-            else
-            {
-              syslog(5, "%s a partir del caracter %d\n ", sequences_to_analyze[i].sequence, sequences_to_analyze[i].found_at);
-              snprintf(sReportLine, sizeof(sReportLine), "%s a partir del caracter %d\n ", sequences_to_analyze[i].sequence, sequences_to_analyze[i].found_at);
-            }
-            strcat(sReport, sReportLine);
-            syslog(5, "sReportLine=%s", sReportLine);
-          }
-          syslog(5, "sReport:\n%s", sReport);
+            break;
 
-          reportSize = strlen(sReport);
-          serverReplyP = malloc(reportSize * sizeof(char));
-          
-          snprintf(serverReplyP, ++reportSize, "\n%s", sReport  );
-          send(new_socket, serverReplyP, ++reportSize, 0);
-          free(sReport);
-          break;
+          case 'S':  // LOAD NEW SEQUENCES
+            cantSeq = 0;
+            clientMsg[iLen - 1] = '\0';
+
+            sequenceSize = countChar(clientMsg);
+            secuencia = malloc(sequenceSize * sizeof(char));
+
+            FILE *ptrS = fopen(clientMsg, "r");
+
+            if (ptrS == NULL)
+              syslog(LOG_ERR, "Could no open selected file\n");
+            else {
+              while (fgets(secuencia, sequenceSize, ptrS)) {
+                /*
+                    AQUI HAY GUARDAMOS CADA secuencia EN UN STRUCT
+                  */
+                int read_len = strlen(secuencia);
+                if (secuencia[read_len - 1] == '\n') {
+                  secuencia[read_len - 1] = 0;
+                  secuencia[read_len - 2] = 0;
+                }
+                int true_len = strlen(secuencia);
+                // syslog(LOG_NOTICE, "strlen secuencia = %d", true_len);
+                // syslog(LOG_NOTICE, "Secuencia[%d] = %s", cantSeq, secuencia);
+                // syslog(LOG_NOTICE, "Secuencia[%d] = %s", cantSeq, secuencia);
+                sequences_to_analyze[cantSeq].sequence =
+                    malloc(true_len * sizeof(char));
+                strcpy(sequences_to_analyze[cantSeq].sequence, secuencia);
+                sequences_to_analyze[cantSeq].length = true_len;
+                cantSeq++;
+              }
+              fclose(ptrS);
+            }
+            free(secuencia);
+            // Begin multithread with Open MP
+            omp_set_num_threads(cantSeq);
+
+#pragma omp parallel for
+            for (int i = 0; i < cantSeq; i++) {
+              sequences_to_analyze[i].found_at_reference_address =
+                  strstr(reference, sequences_to_analyze[i].sequence);
+              if (sequences_to_analyze[i].found_at_reference_address != NULL)
+                sequences_to_analyze[i].found_at =
+                    sequences_to_analyze[i].found_at_reference_address -
+                    reference;
+              else
+                sequences_to_analyze[i].found_at = -1;  // SEQUENCE NOT FOUND
+            }
+            // Sequential code begins again
+            char *sReport = malloc(10000);
+            memset(sReport, '\0', sizeof(sReport));
+            for (int i = 0; i < cantSeq; i++) {
+              char sReportLine[sequences_to_analyze[i].length +
+                               40];  // Buffer for each line that must be
+                                     // printed in report
+              if (sequences_to_analyze[i].found_at == -1) {
+                syslog(5, "%s no se encontro\n",
+                       sequences_to_analyze[i].sequence);
+                snprintf(sReportLine, sizeof(sReportLine),
+                         "%s no se encontro\n",
+                         sequences_to_analyze[i].sequence);
+              } else {
+                syslog(5, "%s a partir del caracter %d\n ",
+                       sequences_to_analyze[i].sequence,
+                       sequences_to_analyze[i].found_at);
+                snprintf(sReportLine, sizeof(sReportLine),
+                         "%s a partir del caracter %d\n ",
+                         sequences_to_analyze[i].sequence,
+                         sequences_to_analyze[i].found_at);
+              }
+              strcat(sReport, sReportLine);
+              syslog(5, "sReportLine=%s", sReportLine);
+            }
+            syslog(5, "sReport:\n%s", sReport);
+
+            reportSize = strlen(sReport);
+            serverReplyP = malloc(reportSize * sizeof(char));
+
+            snprintf(serverReplyP, ++reportSize, "\n%s", sReport);
+            send(new_socket, serverReplyP, ++reportSize, 0);
+            free(sReport);
+            break;
+          default:
+            snprintf(serverReply, sizeof(serverReply),
+                     "Sorry, please type '1' or '2', nothing else.\n");
+            send(new_socket, serverReply, sizeof(serverReply), 0);
+            break;
         }
       }
       free(reference);
     }
-    if (new_socket < 0)
-    {
+    if (new_socket < 0) {
       syslog(LOG_ERR, "accept failed");
       exit(1);
     }
-    if (signal(SIGTERM, kill_handler) == SIG_ERR)
-    {
+    if (signal(SIGTERM, kill_handler) == SIG_ERR) {
       syslog(LOG_NOTICE, "Error terminating process.");
       exit(1);
     }
